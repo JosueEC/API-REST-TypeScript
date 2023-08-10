8f8200a88d3513018820fdf23391e0df6e9c3df5
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-08-09 20:22:56 -0600

    feat(services): 🔨 Add NOT_FOUND error handlers to the user services

--- src/services/item.service.ts
+++ src/services/item.service.ts
@@ -1,34 +1,40 @@
 import { Car } from '../interfaces/car.interface'
 import ItemModel from '../models/item.model'
 
-const insertCar = async (item: Car) => {
+const insertCar = async (item: Car): Promise <Car> => {
   const responseInsert = await ItemModel.create(item)
   return responseInsert
 }
 
-const getCars = async () => {
+const getCars = async (): Promise <Car[]> => {
   const responseItems = await ItemModel.find({})
   return responseItems
 }
 
-const getCar = async (id: string) => {
+const getCar = async (id: string): Promise <Car> => {
   const responseItem = await ItemModel.findById({ _id: id })
+
+  if (responseItem == null) throw new Error('CAR_NOT_FOUND')
   return responseItem
 }
 
-const updateCar = async (id: string, data: Car) => {
+const updateCar = async (id: string, data: Car): Promise <Car> => {
   const responseItem = await ItemModel.findByIdAndUpdate(
-    { _id: id},
+    { _id: id },
     data,
     {
       new: true
     }
   )
+
+  if (responseItem == null) throw new Error('CAR_NOT_FOUND')
   return responseItem
 }
 
-const deleteCar = async (id: string) => {
+const deleteCar = async (id: string): Promise <Car> => {
   const responseItem = await ItemModel.findOneAndRemove({ _id: id })
+
+  if (responseItem == null) throw new Error('CAR_NOT_FOUND')
   return responseItem
 }
 
