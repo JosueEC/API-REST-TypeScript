882bba0f82d9d49297ce6aef42013bab5fe326da
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-08-14 21:13:19 -0600

    fix(controllers): 🔨 Change response format in the PostController

--- src/controllers/post.ts
+++ src/controllers/post.ts
@@ -10,34 +10,34 @@ import {
 
 const getPosts = (_req: Request, res: Response): void => {
   findPosts()
-    .then((response) => res.status(200).send({ response }))
+    .then((response) => res.status(200).send(response))
     .catch((error) => handleHTTP(res, 'ERROR_GET_POSTS', error))
 }
 
 const getPostByID = ({ params }: Request, res: Response): void => {
   const { id } = params
   findOnePost(id)
-    .then((response) => res.status(200).send({ response }))
+    .then((response) => res.status(200).send(response))
     .catch((error) => handleHTTP(res, 'ERROR_FIND_USER', error))
 }
 
 const createPost = ({ body }: Request, res: Response): void => {
   savePost(body)
-    .then((response) => res.status(200).send({ response }))
+    .then((response) => res.status(200).send(response))
     .catch((error) => handleHTTP(res, 'ERROR_CREATE_POST', error))
 }
 
 const putPost = ({ params, body }: Request, res: Response): void => {
   const { id } = params
   updatePost(id, body)
-    .then((response) => res.status(200).send({ response }))
+    .then((response) => res.status(200).send(response))
     .catch((error) => handleHTTP(res, 'ERROR_UPDATE_POST', error))
 }
 
 const deletePost = ({ params }: Request, res: Response): void => {
   const { id } = params
   removePost(id)
-    .then((response) => res.status(200).send({ response }))
+    .then((response) => res.status(200).send(response))
     .catch((error) => handleHTTP(res, 'ERROR_DELETE_POST', error))
 }
 
